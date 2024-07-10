84741d4a86dc54ed44759d92c0877cbb3f790f54
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2024-07-10 11:46:01 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const thead = document.querySelectorAll("thead th");
+for(let i = 0; i < thead.length; i++)thead[i].addEventListener("click", ()=>{
+    sortByColumnIndex(i);
+});
+const tbody = document.querySelector("table tbody");
+const rows = [
+    ...tbody.rows
+];
+function sortByColumnIndex(index) {
+    rows.sort((rowA, rowB)=>{
+        const cellA = rowA.cells[index].textContent;
+        const cellB = rowB.cells[index].textContent;
+        return cellA.localeCompare(cellB, undefined, {
+            numeric: true
+        });
+    });
+    tbody.innerHTML = "";
+    rows.forEach((row)=>tbody.append(row));
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
